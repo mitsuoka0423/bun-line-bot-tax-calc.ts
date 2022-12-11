31489fa575a7816610cadb1ec79c56aa1bd237a1
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-12-12 03:09:15 +0900

    chore: 環境変数未入力時をエラーに変更

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { Hono } from "hono";
 import { getCallback, postCallback } from "./routes";
 import { prettyJSON } from 'hono/pretty-json';
 
+const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
+if (!channelAccessToken)
+  throw new Error("チャンネルアクセストークンを入力してください。");
+
+const channelSecret = process.env.CHANNEL_SECRET;
+if (!channelSecret)
+  throw new Error("チャンネルシークレットを入力してください。");
+
 const port = parseInt(process.env.PORT) || 3000;
 
 const app = new Hono();
