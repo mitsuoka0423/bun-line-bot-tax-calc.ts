9862c584961e21e73a6382fecc4d268b2bf04018
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-12-13 07:02:08 +0900

    refactor: 税金計算処理を関数に切り出し

--- src/services/payAmountCalculatorService.ts
+++ src/services/payAmountCalculatorService.ts
@@ -5,16 +5,52 @@ import { SalesAmountWithoutSalesTax } from "../domains/SalesAmountWithoutSalesTa
 import { SalesTaxAmount } from "../domains/SalesTaxAmount";
 import { WithholdingIncomeTaxAmount } from "../domains/WithholdingIncomeTaxAmount";
 
-interface TaxIncludedPriceServiceProps {
+interface PayAmountCalculatorServiceProps {
   price: number;
 }
 
 export const payAmountCalculatorService = async ({
   price,
-}: TaxIncludedPriceServiceProps): Promise<Message[]> => {
+}: PayAmountCalculatorServiceProps): Promise<Message[]> => {
   console.log("[START] payAmountCalculatorService");
   console.log({ price });
 
+  const {
+    salesAmountWithSalesTax,
+    salesTaxAmount,
+    salesAmountWithoutSalesTax,
+    withholdingIncomeTaxAmount,
+    paymentAmount,
+  } = calculatePayAmountWithTaxIncludedAmount(price);
+
+  const messages: Message[] = [
+    {
+      type: "text",
+      text: [
+        `内税　　　：${salesAmountWithSalesTax.value}`,
+        `消費税　　：${salesTaxAmount.value}`,
+        `外税　　　：${salesAmountWithoutSalesTax.value}`,
+        `源泉徴収税：${withholdingIncomeTaxAmount.value}`,
+        `振込金額　：${paymentAmount.value}`,
+      ].join("\n"),
+    },
+  ];
+
+  console.log({ message: messages });
+  console.log("[END  ] payAmountCalculatorService");
+
+  return messages;
+};
+
+const calculatePayAmountWithTaxIncludedAmount = (
+  price
+): {
+  salesAmountWithSalesTax: SalesAmountWithSalesTax;
+  salesTaxAmount: SalesTaxAmount;
+  salesAmountWithoutSalesTax: SalesAmountWithoutSalesTax;
+  withholdingIncomeTaxAmount: WithholdingIncomeTaxAmount;
+  paymentAmount: PaymentAmount;
+} => {
   const salesAmountWithSalesTax = new SalesAmountWithSalesTax(price);
   const salesTaxAmount = new SalesTaxAmount(salesAmountWithSalesTax, 0.1);
   const salesAmountWithoutSalesTax = new SalesAmountWithoutSalesTax(
@@ -30,19 +66,11 @@ export const payAmountCalculatorService = async ({
     withholdingIncomeTaxAmount
   );
 
-  const messages: Message[] = [{
-    type: "text",
-    text: [
-      `内税　　　：${salesAmountWithSalesTax.value}`,
-      `消費税　　：${salesTaxAmount.value}`,
-      `外税　　　：${salesAmountWithoutSalesTax.value}`,
-      `源泉徴収税：${withholdingIncomeTaxAmount.value}`,
-      `振込金額　：${paymentAmount.value}`,
-    ].join("\n"),
-  }];
-
-  console.log({ message: messages });
-  console.log("[END  ] payAmountCalculatorService");
-
-  return messages;
+  return {
+    salesAmountWithSalesTax,
+    salesTaxAmount,
+    salesAmountWithoutSalesTax,
+    withholdingIncomeTaxAmount,
+    paymentAmount,
+  };
 };
